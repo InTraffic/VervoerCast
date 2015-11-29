644ab53e3e273ec01e5d53bffbde5f32d2090b57
Frans Slothouber <rfsber@xs4all.nl>
2015-11-29 21:25:40 +0100

    Extract both kinds of forecasts

--- DataCollectors/WeatherForecast/parse_forecast.py
+++ DataCollectors/WeatherForecast/parse_forecast.py
@@ -1,10 +1,18 @@
 from bs4 import BeautifulSoup
 import sys
 import datetime
+import logging
 
+def extract_forecast_text(soup):
+    lines = []
+    for d in soup.find_all('div'):
+        if 'class' in d.attrs:
+            if 'weather__text' in d['class']:
+                for ss in d.strings:
+                    lines.append(ss)
+    return("\n".join(lines))
 
-def foo(filename):
-    soup = BeautifulSoup(open(filename), "lxml")
+def extract_forecast_table(soup):
     forecast = []
     for x in soup.find_all('ul'):
         if 'class' in x.attrs:
@@ -22,14 +30,31 @@ def foo(filename):
     return(forecast)
 
 
-tomorrow = datetime.date.today()+datetime.timedelta(days=1)
+if __name__ == "__main__":
+    today    = datetime.date.today()
 
-forecast = foo(sys.argv[1])
-tomorrows_forecast = forecast[0]
-# Check if the forecast is indeed for tomorrow.
-ist_date = datetime.datetime.strptime(tomorrows_forecast[0], "%d-%m-%Y")
-if tomorrow == ist_date.date():
-    print(" ".join(tomorrows_forecast))
-else:
-    pass
+    # Parse the forecast page
+    soup = BeautifulSoup(open(sys.argv[1]), "lxml")
+
+    # Extract the written forecast.
+    forecast_text = extract_forecast_text(soup)
+
+    # Extract data from the table in the forecast.
+    forecast = extract_forecast_table(soup)
+
+    tomorrows_forecast = forecast[0]
+    tomorrow = today + datetime.timedelta(days=1)
+
+    # Check if the forecast is indeed for tomorrow.
+    ist_date = datetime.datetime.strptime(
+            tomorrows_forecast[0], "%d-%m-%Y")
+    if tomorrow == ist_date.date():
+        datastring = " ".join(tomorrows_forecast)
+    else:
+        datastring = "NA"
+        pass
+
+    datastring = str(today) + ',"' + datastring + '"'
+    print(datastring)
+    print(forecast_text)
 
