cb0c5bfc5609a825de12b12d73ae61cb40049808
Frans Slothouber <rfsber@xs4all.nl>
2015-12-03 14:08:00 +0100

    Activate uses source not ./

--- DataCollectors/WeatherForecast/fetch_forecast.sh
+++ DataCollectors/WeatherForecast/fetch_forecast.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cd ~/.virtualenvs/vervoercast/
+source ./bin/activate
 cd /tmp
 rm -f verwachtingen
 wget http://www.knmi.nl/nederland-nu/weer/verwachtingen
 python ~/bin/parse_forecast.py verwachtingen
+
 
